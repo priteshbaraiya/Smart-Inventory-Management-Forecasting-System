b417291fd74aa17bd282faa8bb8407fb092c0199
priteshbaraiya <priteshbaraiya75@gmail.com>
2025-12-25 09:27:36 +0530

    Initial commit - Django project setup

---
+++ hello/home/views.py
@@ -0,0 +1,7 @@
+
+from django.shortcuts import render,HttpResponse
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("This is homepage")
